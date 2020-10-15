21730aa10038ec620f11522e3a35d004a0e2edc6
Alan Truong <truong@revu.net>
2020-10-15 17:04:57 +0700

    add modal component

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  private isShow: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleModal() {
+    this.isShow = !this.isShow;
+  }
+
+}
